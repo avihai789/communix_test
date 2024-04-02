00643d8de70ed78e8679ed005a626bbf49c27ef5
Avihai Zeligman <avihai789@gmail.com>
2024-04-03 01:53:41 +0300

    firebase integration

---
+++ Assets/BlockBreakerTemplate/Scripts/RemoteConfig.cs
@@ -0,0 +1,52 @@
+using Firebase.Extensions;
+using Firebase.RemoteConfig;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class RemoteConfig : MonoBehaviour
+{
+    private void Awake()
+    {
+        CheckRemoteConfigValues();
+    }
+
+    public Task CheckRemoteConfigValues()
+    {
+        Debug.Log("Fetching data...");
+        Task fetchTask = FirebaseRemoteConfig.DefaultInstance.FetchAsync(TimeSpan.Zero);
+        return fetchTask.ContinueWithOnMainThread(FetchComplete);
+    }
+    private void FetchComplete(Task fetchTask)
+    {
+        if (!fetchTask.IsCompleted)
+        {
+            Debug.LogError("Retrieval hasn't finished.");
+            return;
+        }
+
+        var remoteConfig = FirebaseRemoteConfig.DefaultInstance;
+        var info = remoteConfig.Info;
+        if (info.LastFetchStatus != LastFetchStatus.Success)
+        {
+            Debug.LogError($"{nameof(FetchComplete)} was unsuccessful\n{nameof(info.LastFetchStatus)}: {info.LastFetchStatus}");
+            return;
+        }
+
+        remoteConfig.ActivateAsync()
+          .ContinueWithOnMainThread(
+            task => {
+                Debug.Log($"Remote data loaded and ready for use. Last fetch time {info.FetchTime}.");
+
+                print("Total values: "+remoteConfig.AllValues.Count);
+
+                foreach (var item in remoteConfig.AllValues)
+                {
+                    print("Key :" + item.Key);
+                    print("Value: " + item.Value.StringValue);
+                }
+
+            });
+    }
+
+}
